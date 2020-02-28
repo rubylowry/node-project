3b1d594983bba06b93b5b9064cca19a316028e5c
Ubuntu <vagrant@ubuntu-xenial.localdomain>
2020-02-28 00:04:04 +0000

    linked node_modules and css

--- server.js
+++ server.js
@@ -25,38 +25,60 @@ const server = http.createServer((req, res) =>{
 			});
 		}
 		// If the user is trying to access the about page on the website
+
 		else if(req.url === '/about.html'){
 			fs.readFile('./public/about.html' , 'UTF-8' , (err,data) =>{
 				if(err) throw err;
 				res.writeHead(200, {'Content-Type' : 'text/html'});
 				res.end(data);
-			});
-		}
+			})
+		         }else if (req.url.match ('/node_modules/')) {
+			       const nodePath = path.join(__dirname, req.url);
+			       fs.readFile(nodePath , 'UTF-8' , (err,data) =>{
+				       if(err) throw err;
+				       res.writeHead(200, {'Content-Type' : 'text/css'});
+				       res.end(data);
+						 })
+					 // Gets the css for the pages
+					 } else if (req.url.match ('/css/')) {
+			                    const cssPath = path.join(__dirname, 'public', req.url);
+			                    fs.readFile(cssPath , 'UTF-8' , (err,data) =>{
+				                    if(err) throw err;
+				                    res.writeHead(200, {'Content-Type' : 'text/css'});
+				                    res.end(data);
+							})
+          }
 		// If the user is trying to access the index page from any other page on the site
 		else if(req.url === '/index.html'){
 			fs.readFile('./public/index.html' , 'UTF-8' , (err,data) =>{
 				if(err) throw err;
 				res.writeHead(200, {'Content-Type' : 'text/html'});
 				res.end(data);
-			});
+			})
+		         } else if (req.url.match ('/node_modules/')) {
+			         const nodePath = path.join(__dirname, req.url);
+			         fs.readFile(nodePath , 'UTF-8' , (err,data) =>{
+				         if(err) throw err;
+				         res.writeHead(200, {'Content-Type' : 'text/css'});
+				         res.end(data);
+			         })
 		}
 		else if(req.url === '/contactPage.html'){
 			fs.readFile('./public/contactPage.html' , 'UTF-8' , (err,data) =>{
 				if(err) throw err;
 				res.writeHead(200, {'Content-Type' : 'text/html'});
 				res.end(data);
-			});
+			})
+		          } else if (req.url.match ('/node_modules/')) {
+			          const nodePath = path.join(__dirname, req.url);
+			          fs.readFile(nodePath , 'UTF-8' , (err,data) =>{
+				          if(err) throw err;
+				          res.writeHead(200, {'Content-Type' : 'text/css'});
+				          res.end(data);
+			          })
 		}
-		// Gets the css for the pages
-		// else if(req.url === '/stylesheet.css'){
-			// fs.readFile('./public/css/stylesheet.css' , (err,data) =>{
-				// if(err) throw err;
-				// res.writeHead(200, {'Content-Type' : 'text/css'});
-				// res.end(data);
-			// });
-		// }
-	} // Method ends here
-});
+	}
+});  // Method ends here
 
 // Usually use port 3000 for setting up our http server
 server.listen(3000);
