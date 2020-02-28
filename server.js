fabc8f79c9931527d629ca57ebbf4db9e194a352
Ubuntu <vagrant@ubuntu-xenial.localdomain>
2020-02-28 02:17:08 +0000

    entering data and recieving it in the server

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-// Core modules - Provides server
-const http = require('http');
-// use this for file transaction
-const fs = require('fs');
-const path = require('path');
+//core modules
+ const http = require('http'); //provides server
+ const fs = require('fs'); //use this for file transaction
+ const path = require('path');
+ const qs = require('querystring');
 
 const server = http.createServer((req, res) =>{
 	// res.writeHead(200, {'Content-Type' : 'text/plain'});
@@ -11,7 +11,7 @@ const server = http.createServer((req, res) =>{
 
 	console.log(`${req.method} request for ${req.url}`)
 
-	// If the user is trying to access a site
+	// If the user is trying to access a page
 	if(req.method === 'GET'){
 		// If the user is trying to view the index
 		if(req.url === '/'){
@@ -32,7 +32,7 @@ const server = http.createServer((req, res) =>{
 				res.writeHead(200, {'Content-Type' : 'text/html'});
 				res.end(data);
 			})
-		         }else if (req.url.match ('/node_modules/')) {
+		         } else if (req.url.match ('/node_modules/')) {
 			       const nodePath = path.join(__dirname, req.url);
 			       fs.readFile(nodePath , 'UTF-8' , (err,data) =>{
 				       if(err) throw err;
@@ -47,6 +47,20 @@ const server = http.createServer((req, res) =>{
 				                    res.writeHead(200, {'Content-Type' : 'text/css'});
 				                    res.end(data);
 							})
+						} else if (req.url.match ('/img/')) {
+																				const imgPath = path.join(__dirname, 'public', req.url);
+																				fs.readFile(imgPath , 'UTF-8' , (err,data) =>{
+																					if(err) throw err;
+																					res.writeHead(200, {'Content-Type' : 'image/jpeg'});
+																					res.end(data);
+														})
+													} else if (req.url.match ('/js/')) {
+																											const jsPath = path.join(__dirname, 'public', req.url);
+																											fs.readFile(jsPath , 'UTF-8' , (err,data) =>{
+																												if(err) throw err;
+																												res.writeHead(200, {'Content-Type' : 'text/js'});
+																												res.end(data);
+																					})
           }
 		// If the user is trying to access the index page from any other page on the site
 		else if(req.url === '/index.html'){
@@ -62,6 +76,28 @@ const server = http.createServer((req, res) =>{
 				         res.writeHead(200, {'Content-Type' : 'text/css'});
 				         res.end(data);
 			         })
+							 // Gets the css for the pages
+							 } else if (req.url.match ('/css/')) {
+					                    const cssPath = path.join(__dirname, 'public', req.url);
+					                    fs.readFile(cssPath , 'UTF-8' , (err,data) =>{
+						                    if(err) throw err;
+						                    res.writeHead(200, {'Content-Type' : 'text/css'});
+						                    res.end(data);
+									})
+								} else if (req.url.match (/images/)) {
+																						const imgPath = path.join(__dirname, 'public', req.url);
+																						fs.readFile(imgPath, (err,data) =>{
+																							if(err) throw err;
+																							res.writeHead(200, {'Content-Type' : 'image/jpg'});
+																							res.end(data);
+																})
+															} else if (req.url.match ('/js/')) {
+																													const jsPath = path.join(__dirname, 'public', req.url);
+																													fs.readFile(jsPath , 'UTF-8' , (err,data) =>{
+																														if(err) throw err;
+																														res.writeHead(200, {'Content-Type' : 'text/js'});
+																														res.end(data);
+																							})
 		}
 		else if(req.url === '/contactPage.html'){
 			fs.readFile('./public/contactPage.html' , 'UTF-8' , (err,data) =>{
@@ -76,9 +112,52 @@ const server = http.createServer((req, res) =>{
 				          res.writeHead(200, {'Content-Type' : 'text/css'});
 				          res.end(data);
 			          })
-		}
-	}
-});  // Method ends here
+								// Gets the css for the pages
+ 							         } else if (req.url.match ('/css/')) {
+ 					                            const cssPath = path.join(__dirname, 'public', req.url);
+ 					                            fs.readFile(cssPath , 'UTF-8' , (err,data) =>{
+ 						                            if(err) throw err;
+ 						                            res.writeHead(200, {'Content-Type' : 'text/css'});
+ 						                            res.end(data);
+ 							        		})
+												} else if (req.url.match ('/img/')) {
+												            								const imgPath = path.join(__dirname, 'public', req.url);
+												            								fs.readFile(imgPath , 'UTF-8' , (err,data) =>{
+												            									if(err) throw err;
+												            									res.writeHead(200, {'Content-Type' : 'image/jpeg'});
+												            									res.end(data);
+												            		})
+																			} else if (req.url.match ('/js/')) {
+																																	const jsPath = path.join(__dirname, 'public', req.url);
+																																	fs.readFile(jsPath , 'UTF-8' , (err,data) =>{
+																																		if(err) throw err;
+																																		res.writeHead(200, {'Content-Type' : 'text/js'});
+																																		res.end(data);
+																											})
+		                                                }
+
+																									} else if (req.method === 'POST'){
+
+ 																							     if (req.url === '/sendForm') {
+ 																							       // console.log('form submitted');
+ 																							       let body = '';
+
+ 																							       req.on('data', function(data){
+ 																							         body +=data;
+ 																							         // body = body + data
+ 																							       });
+
+ 																							       req.on('end', function(){
+ 																							         console.log('form data ends');
+ 																							         console.log(body.toString());
+																											 const formData = qs.parse(body.toString());
+																							         console.log(formData);
+
+ 																							       })
+
+ 																							     }
+ 																							   }
+     });  // Method ends here
 
 // Usually use port 3000 for setting up our http server
 server.listen(3000);
